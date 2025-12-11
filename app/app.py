765ddf68a4b1f37417052f26471a6b2d58335a36
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-12-11 12:15:00 +0000

    feat: introduce check for json report presence

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from app.notifier_type.discord import discord_notification
 from settings import Settings
+from utils.common import err
 
 
 def run_notifier(settings: Settings) -> int:
@@ -11,6 +12,12 @@ def run_notifier(settings: Settings) -> int:
     :return: software exit code
     :rtype: int
     """
+
+    if not settings.report_json.exists():
+        err("Can't resolve the json report in the path location: ",
+            str(settings.report_json))
+
+        return 1
 
     if settings.discord_webhook_url:
         return discord_notification(settings)
