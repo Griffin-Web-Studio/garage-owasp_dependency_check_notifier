13bd6233340c789736e18588b700ac2068c72ee8
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-12-13 21:42:18 +0000

    feat(DCParser): complete data loader method by including json loading and validation logic

--- app/DCParser.py
+++ app/DCParser.py
@@ -2,11 +2,17 @@
 Class holding the primary parsing logic of the OWASP Dependency Checker report
 """
 
+import json
+from typing import Any, Dict, List, Optional
+from pydantic import ValidationError
+from app.models.report_models import DCModel
+
 from settings import Settings
+from utils.common import err
 
 
 class DCParser:
-    _data: dict[str, str] | None
+    _report: Optional[DCModel] = None
     _settings: Settings
 
     def __init__(self, settings: Settings):
@@ -14,13 +20,21 @@ class DCParser:
         Initialises the parser with the source data.
         """
         self._settings = settings
+        self._load_data()
         # self._data = self._parse()
 
     def _load_data(self):
         """
         Loads the source data. Override this method in subclasses.
         """
-        raise NotImplementedError("Subclasses should implement this method.")
+        try:
+            raw = json.loads(self._settings.report_json.read_text())
+            self._report = DCModel.model_validate(raw)
+        except ValidationError:
+            err(
+                "Could not correctly validate the report schema against the",
+                "known model."
+            )
 
     def _parse(self):
         """
